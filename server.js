f1cf7c1072f0d0685b41475881688383f6685f9e
Reaperz0z <raakesh.balaji@gmail.com>
2021-02-21 14:36:01 -0500

    formatted

--- server.js
+++ server.js
@@ -7,61 +7,62 @@ const Port = process.env.PORT || 8080;
 calli.initialize().then(function () {
     app.listen(Port, () => {
         console.log("Listening on this port: " + Port);
-        app.get("/employees", (req, res) => {
-            if (typeof req.query.department != "undefined") {
-                calli.getEmployeesbyDepartment(req.query.department).then(function (data) { res.send(data); }).catch(() => {
-                    res.json({ message: "no results" })
-                })
-            }
-            else {
-                calli.getAllEmployees().then(function (data) { res.send(data) }).catch(() => {
-                    res.json({ message: "no results" });
-                })
-            }
-        });
-
-        app.get("/departments", (req, res) => {
-            calli.getDepartments().then(function (data) { res.send(data); }).catch((data) => {
+    })
+    app.get("/employees", (req, res) => {
+        if (typeof req.query.department != "undefined") {
+            calli.getEmployeesbyDepartment(req.query.department).then(function (data) { res.send(data); }).catch(() => {
                 res.json({ message: "no results" })
-            });
+            })
+        }
+        else {
+            calli.getAllEmployees().then(function (data) { res.send(data) }).catch(() => {
+                res.json({ message: "no results" });
+            })
+        }
+    });
 
+    app.get("/departments", (req, res) => {
+        calli.getDepartments().then(function (data) { res.send(data); }).catch((data) => {
+            res.json({ message: "no results" })
         });
 
-        app.get("/managers", (req, res) => {
-            calli.getManagers().then(function (data) { res.send(data); }).catch((data) => {
-                res.json({ message: "no results" })
-            });
+    });
 
-
+    app.get("/managers", (req, res) => {
+        calli.getManagers().then(function (data) { res.send(data); }).catch((data) => {
+            res.json({ message: "no results" })
         });
-
-
-        app.get("/employee/:num", (req, res) => {
-            calli.getEmployeesbyNum(req.params.num).then(function (data) { res.send(data); }).catch((data) => {
-                res.send(data)
-            });
-
-        });
-
-        app.get("/", (req, res) => {
-            res.sendFile(path.join(__dirname, "/views/home.html"));
-        })
-
-        app.get("/about", (req, res) => {
-            res.sendFile(path.join(__dirname, "/views/about.html"));
-        })
-
-        app.get("/htmlDemo", (req, res) => {
-            res.sendFile(path.join(__dirname, "/views/htmlDemo.html"));
-        })
-
-
-        app.get("/:nonsense", (req, res) => {
-            res.status(404).sendFile(path.join(__dirname, "download.jpg"));
-        })
 
 
     });
-}).catch(function () {
+
+
+    app.get("/employee/:num", (req, res) => {
+        calli.getEmployeesbyNum(req.params.num).then(function (data) { res.send(data); }).catch((data) => {
+            res.send(data)
+        });
+
+    });
+
+    app.get("/", (req, res) => {
+        res.sendFile(path.join(__dirname, "/views/home.html"));
+    })
+
+    app.get("/about", (req, res) => {
+        res.sendFile(path.join(__dirname, "/views/about.html"));
+    })
+
+    app.get("/htmlDemo", (req, res) => {
+        res.sendFile(path.join(__dirname, "/views/htmlDemo.html"));
+    })
+
+
+    app.get("/:nonsense", (req, res) => {
+        res.status(404).sendFile(path.join(__dirname, "download.jpg"));
+    })
+
+
+
+}).catch(function (err) {
     console.log("Server Cannot be started!! Error at Initializing data!");
 })
